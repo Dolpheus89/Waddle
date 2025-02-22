e5c24b8bf3099e82b5d68cf1d482a36b135905c8
Florian Schaessens <f.schaessens@gmail.com>
2025-02-22 20:43:48 +0100

    feature Button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
-import "./App.css"
+import "./App.css";
+import { useEffect, useState } from "react";
 import { Button } from "./lib";
 
 function App() {
+	const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+	useEffect(() => {
+		document.documentElement.setAttribute("data-theme", theme);
+	}, [theme]);
+
+	const toggleTheme = () => {
+		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+	};
+
 	return (
 		<main>
-			<Button>Button</Button>
+			<button type="button" onClick={toggleTheme}>
+				{theme === "light" ? "Mode Sombre" : "Mode Clair"}
+			</button>
+			<div style={{ display: "flex", alignItems: "end" }}>
+				<Button size="sm" variant="text">
+					Text
+				</Button>
+				<Button size="m" variant="text">
+					Text
+				</Button>
+				<Button size="l" variant="text">
+					Text
+				</Button>
+				<Button size="l" variant="text" disabled>
+					Text
+				</Button>
+			</div>
+			<div style={{ display: "flex", alignItems: "end" }}>
+				<Button size="sm" variant="contained">
+					Contained
+				</Button>
+				<Button size="m" variant="contained">
+					Contained
+				</Button>
+				<Button size="l" variant="contained">
+					Contained
+				</Button>
+				<Button size="l" variant="contained" disabled>
+					Contained
+				</Button>
+			</div>
+			<div
+				style={{ display: "flex", alignItems: "end", justifyContent: "start" }}
+			>
+				<Button size="sm" variant="outlined">
+					Outlined
+				</Button>
+				<Button size="m" variant="outlined">
+					Outlined
+				</Button>
+				<Button size="l" variant="outlined">
+					Outlined
+				</Button>
+				<Button size="l" variant="outlined" disabled>
+					Oulined
+				</Button>
+			</div>
 		</main>
 	);
 }
--- src/lib/components/atoms/Button/Button.tsx
+++ src/lib/components/atoms/Button/Button.tsx
@@ -1,13 +1,44 @@
-import style from "./Button.module.scss"
+import classNames from "classnames";
+import styles from "./Button.module.scss";
 
 export type ButtonProps = {
-	children?: string;
+	children: string;
+	variant?: "outlined" | "text" | "contained";
+	size?: "sm" | "m" | "l";
+	color?: "primary" | "secondary" | "success" | "danger";
+	className?: string;
+	/** Indicates if the button is disabled */
+	disabled?: boolean;
+	/** Indicates if the button is in a loading state */
+	loading?: boolean;
 };
 
 /** A button */
-export function Button({children} : ButtonProps) {
+export function Button({
+	children,
+	variant = "contained",
+	size = "m",
+	color = "primary",
+	className,
+	disabled,
+	loading = false,
+	...rest
+}: ButtonProps) {
 	return (
-		<button type="button" className={style.button}>
+		<button
+			type="button"
+			className={classNames(
+				styles.button,
+				styles[variant],
+				styles[size],
+				styles[color],
+				loading && styles.loading,
+				className,
+			)}
+			disabled={disabled || loading}
+			aria-busy={loading}
+			{...rest}
+		>
 			{children}
 		</button>
 	);
